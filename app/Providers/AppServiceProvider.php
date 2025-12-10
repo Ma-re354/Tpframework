66e9cd0a08bc956c987eba34d99e33a79016a8cb
TIANRY Maréol <tianrymareol330@gmail.com>
2025-12-10 13:01:57 +0100

    Commt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,10 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
-
-        if (env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
