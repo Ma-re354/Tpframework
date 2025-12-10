9201a5baa2e76717c5529651c2d1cdda6e20f1d9
TIANRY Maréol <tianrymareol330@gmail.com>
2025-12-10 15:26:15 +0100

    Nouvt

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Note: Composer install est déjà fait dans le Dockerfile, 
-# mais on peut le refaire ici par sécurité ou pour les scripts post-install.
-
 echo "Caching config..."
 php artisan config:cache
 php artisan route:cache
@@ -12,6 +9,8 @@ php artisan view:cache
 echo "Running migrations..."
 php artisan migrate --force
 
+echo "Running seeders..."
+php artisan db:seed --force
+
 echo "Starting Apache..."
-# Cette commande lance Apache en premier plan
 apache2-foreground
