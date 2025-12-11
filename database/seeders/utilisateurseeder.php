e5ec1bf3a9d267cf71896a698e572ec5dbf83873
TIANRY Maréol <tianrymareol330@gmail.com>
2025-12-11 17:36:32 +0100

    Nouvoott

--- database/seeders/utilisateurseeder.php
+++ database/seeders/utilisateurseeder.php
@@ -20,7 +20,7 @@ class utilisateurseeder extends Seeder
         User::create([
             'nom' => 'COMLAN',
             'prenom' => 'Maurice',
-            'email' => 'maurice.comlan@uacccbabh.bj',
+            'email' => 'maurice.comlan@uacccbabhi.bj',
             'mot_de_passe' => bcrypt('Eneam123'),
             'sexe' => 'F',
             'date_inscription' => Carbon::parse('2024-01-15'),
