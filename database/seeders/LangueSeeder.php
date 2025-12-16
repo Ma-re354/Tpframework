3b5b991cc0c3d98ad360c5ff63eb7a4e45bd95e8
TIANRY Maréol <tianrymareol330@gmail.com>
2025-12-16 12:20:39 +0100

    Nouveau

--- database/seeders/LangueSeeder.php
+++ database/seeders/LangueSeeder.php
@@ -5,27 +5,30 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\langue;
+
+
 
 class LangueSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $langues = [
-            ['nom_langue' => 'Français', 'code_langue' => 'fr', 'description' => 'Français', 'created_at' => now(), 'updated_at' => now()],
-            ['nom_langue' => 'Fon', 'code_langue' => 'Fn', 'description' => 'Fon', 'created_at' => now(), 'updated_at' => now()],
-            ['nom_langue' => 'Bariba', 'code_langue' => 'Br', 'description' => 'Bariba', 'created_at' => now(), 'updated_at' => now()],
-            ['nom_langue' => 'Goun', 'code_langue' => 'Gn', 'description' => 'Goun', 'created_at' => now(), 'updated_at' => now()],
-            ['nom_langue' => 'Tori', 'code_langue' => 'Tr', 'description' => 'Tori', 'created_at' => now(), 'updated_at' => now()],
-            ['nom_langue' => 'Minan', 'code_langue' => 'Mn', 'description' => 'Minan', 'created_at' => now(), 'updated_at' => now()],
-            ['nom_langue' => 'Yoruba', 'code_langue' => 'Yr', 'description' => 'Yoruba', 'created_at' => now(), 'updated_at' => now()],
-          
-            
+            ['nom_langue' => 'Français', 'code_langue' => 'fr', 'description' => 'Français'],
+            ['nom_langue' => 'Fon', 'code_langue' => 'Fn', 'description' => 'Fon'],
+            ['nom_langue' => 'Bariba', 'code_langue' => 'Br', 'description' => 'Bariba'],
+            ['nom_langue' => 'Goun', 'code_langue' => 'Gn', 'description' => 'Goun'],
+            ['nom_langue' => 'Tori', 'code_langue' => 'Tr', 'description' => 'Tori'],
+            ['nom_langue' => 'Minan', 'code_langue' => 'Mn', 'description' => 'Minan'],
+            ['nom_langue' => 'Yoruba', 'code_langue' => 'Yr', 'description' => 'Yoruba'],
         ];
 
-        DB::table('langues')->insertOrIgnore($langues);
+        foreach ($langues as $langue) {
+            Langue::updateOrCreate(
+                ['code_langue' => $langue['code_langue']], // clé unique
+                $langue
+            );
+        }
     }
 }
 
